f0d96be7764e85a05dd93d008c7c8dd54b063bc4
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-20 11:17:04 +0700

    .

---
+++ ProductHomeWork/Model/ListProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductHomeWork.Model
+{
+   public class ListProduct
+    {
+        public string nameProduct { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
