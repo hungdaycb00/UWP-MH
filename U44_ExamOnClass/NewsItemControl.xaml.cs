2b6388449c42b5942caef0175dcccb9a55d242aa
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-20 09:54:03 +0700

    .

---
+++ U44_ExamOnClass/NewsItemControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace U44_ExamOnClass
+{
+    public sealed partial class NewsItemControl : UserControl
+    {
+        public Model.NewItem NewItem { get { return this.DataContext as Model.NewItem; } }
+        public NewsItemControl()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
