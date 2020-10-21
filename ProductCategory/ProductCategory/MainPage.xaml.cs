5bb642b032621961e60a052f964e6a9c5a78a9f8
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-21 20:02:20 +0700

    .

---
+++ ProductCategory/ProductCategory/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ProductCategory.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProductCategory
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Product> Products;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Products = new ObservableCollection<Product>();
+           
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AllProduct.IsSelected)
+            {
+                TitleTextBlock.Text = "All Products";
+                NewManager.GetAll(Products);
+            }
+            if (Iphone.IsSelected)
+            {
+                TitleTextBlock.Text = "Iphone";
+                NewManager.GetNews("Iphone", Products);
+                
+            }
+            else if (Samsung.IsSelected)
+            {
+                TitleTextBlock.Text = "Samsung";
+                NewManager.GetNews("Samsung", Products);
+                
+            }
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MysplitView.IsPaneOpen = !MysplitView.IsPaneOpen;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            AllProduct.IsSelected = true;
+        }
+    }
+}
---
+++ ProductCategory/ProductCategory/Models/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCategory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+    }
+
+    public class NewManager
+    {
+        public static List<Product> GetProduct()
+        {
+            var product = new List<Product>();
+            product.Add(new Product { Id = 1, Name = "Samsung S9",Category = "Samsung", Price = "999", Image = "Assets/s9.png" });
+            product.Add(new Product { Id = 2, Name = "Samsung S10", Category = "Samsung", Price = "999", Image = "Assets/s10.png" });
+            product.Add(new Product { Id = 3, Name = "Samsung S20", Category = "Samsung", Price = "999", Image = "Assets/s20.png" });
+            product.Add(new Product { Id = 4, Name = "Iphone 10", Category = "Iphone", Price = "999", Image = "Assets/i10.png" });
+            product.Add(new Product { Id = 5, Name = "Iphone 11", Category = "Iphone", Price = "999", Image = "Assets/i11.png" });
+            product.Add(new Product { Id = 6, Name = "Iphone 12", Category = "Iphone", Price = "999", Image = "Assets/i12.png" });product.Add(new Product { Id = 1, Name = "Samsung S9",Category = "Samsung", Price = "999", Image = "Assets/s9.png" });
+            product.Add(new Product { Id = 7, Name = "Samsung S10", Category = "Samsung", Price = "999", Image = "Assets/s10.png" });
+            product.Add(new Product { Id = 8, Name = "Samsung S20", Category = "Samsung", Price = "999", Image = "Assets/s20.png" });
+            product.Add(new Product { Id = 9, Name = "Iphone 10", Category = "Iphone", Price = "999", Image = "Assets/i10.png" });
+            product.Add(new Product { Id = 10, Name = "Iphone 11", Category = "Iphone", Price = "999", Image = "Assets/i11.png" });
+            product.Add(new Product { Id = 11, Name = "Iphone 12", Category = "Iphone", Price = "999", Image = "Assets/i12.png" });
+            return product;
+        }
+        public static void GetNews(string category, ObservableCollection<Product> newProduct)
+        {
+            var allItems = GetProduct();
+
+            var filterItems = allItems
+                .Where(p => p.Category == category)
+                .ToList();
+            newProduct.Clear();
+            filterItems.ForEach(p => newProduct.Add(p));
+        }
+        public static void GetAll(ObservableCollection<Product> products)
+        {
+            var allItem = GetProduct();
+            
+            allItem.ForEach(p => products.Add(p));
+        }
+    }
+}
