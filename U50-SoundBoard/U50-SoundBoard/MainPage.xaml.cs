356b4b9ec9f5d5247dae05192868895e369a6b80
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-24 12:04:24 +0700

    .

--- U50-SoundBoard/U50-SoundBoard/MainPage.xaml.cs
+++ U50-SoundBoard/U50-SoundBoard/MainPage.xaml.cs
@@ -8,12 +8,14 @@ using U50_SoundBoard.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.DataTransfer;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -75,6 +77,29 @@ namespace U50_SoundBoard
         {
             var sound = (Sound)e.ClickedItem;
             MyMediElement.Source = new Uri(this.BaseUri, sound.AudioFile);
+        }
+
+        private void SoundGridView_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+
+                if (items.Any())
+                {
+                    var storageFile = items[e] as StorageFile;
+                    var contentType = storageFile.ContentType;
+
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+                    if (contentType == "audio/wav" || contentType == "audio/wav" || contentType == "audio/mpeg")
+                        StorageFile newfile = await storageFile.CopyAsync(folder,
+                            storageFile.Name, NameCollisionOption.GenerateUniqueName);
+                    MyMediElement.SetSource(await storageFile.OpenAsync(FileAccessMode.Read),
+                        contentType);
+                    MyMediElement.Play();
+                }
+            }
         }
     }
 }
---
+++ UWP-WeatherApp/Models/OpenWeatherMapProxy.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+using Windows.Devices.Geolocation;
+
+namespace UWP_WeatherApp.Models
+{
+    public class LocationManager
+    {
+        public async static Task<Geoposition> GetPosition()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus != GeolocationAccessStatus.Allowed) throw new Exception();
+            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+            var position = await geolocator.GetGeopositionAsync();
+            return position;
+        }
+
+    }
+    public class OpenWeatherMapProxy
+    {
+      
+
+        public async static Task<RootObject> GetWeather(double lat, double lon)
+        {
+            
+            var http = new HttpClient();
+            var response = await http.GetAsync("http://api.openweathermap.org/data/2.5/weather?lat=21.027763&lon=105.834160&units=imperial&appid=e884a9d1405ed3d3291b00819931ff32");
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+        }
+        
+       
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public double lat { get; set; }
+    }
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string main { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+    }
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+        [DataMember]
+        public double feels_like { get; set; }
+        [DataMember]
+        public double temp_min { get; set; }
+        [DataMember]
+        public double temp_max { get; set; }
+        [DataMember]
+        public double pressure { get; set; }
+        [DataMember]
+        public double humidity { get; set; }
+    }
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+        [DataMember]
+        public double deg { get; set; }
+    }
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public int type { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public int sunrise { get; set; }
+        [DataMember]
+        public int sunset { get; set; }
+    }
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public Coord coord { get; set; }
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public string @base { get; set; }
+        [DataMember]
+        public Main main { get; set; }
+        [DataMember]
+        public int visibility { get; set; }
+        [DataMember]
+        public Wind wind { get; set; }
+        [DataMember]
+        public Clouds clouds { get; set; }
+        [DataMember]
+        public int dt { get; set; }
+        [DataMember]
+        public Sys sys { get; set; }
+        [DataMember]
+        public int timezone { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public int cod { get; set; }
+    }
+
+
+
+
+}
