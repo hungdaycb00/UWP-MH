1718700e74252a8cd6b69eae7dbe3adf0baa5249
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-10-27 09:03:57 +0700

    exam

---
+++ UWP-Pratice-NongPhanManhHung/UWP-PracticeExam/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_PracticeExam.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageProductPath { get; set; }
+    }
+}
